10886eee2512895d1eb1ebe140cb37ab0b61e237
Frah01 <vinario.francesco@gmail.com>
2022-11-30 16:36:19 +0100

    Entrambi i bonus fatti

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -5,15 +5,30 @@ console.log(x)
 let y = Math.floor(Math.random() * (max - min + 1) + min)
 console.log(y)
 
-    if(x > y){
-        console.log('Il primo giocatore ha vinto')
-    }
 
-    else if (x == y){
-        console.log('Pareggio')
-    }
+const button= document.getElementById('genera')
 
-    else {
-        console.log ('Il secondo giocatore ha vinto')
-    }
+
+
+button.addEventListener('click', function(){
+
+    let primogiocatore = document.getElementById('primo-giocatore')
+    let secondogiocatore = document.getElementById('secondo-giocatore')
+    primogiocatore.innerHTML = ('Primo giocatore:'+" " + x)
+    secondogiocatore.innerHTML = ('Secondo giocatore:'+" " + y)
+    let risultato = document.getElementById('risultato')
+
+    
+        if(x > y){
+            risultato.innerHTML=('Il primo giocatore ha vinto')
+        }
+    
+        else if (x == y){
+            risultato.innerHTML=('Pareggio')
+        }
+    
+        else {
+            risultato.innerHTML=('Il secondo giocatore ha vinto')
+        }
+    })
 
