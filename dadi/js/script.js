95137403efdbfbed4506665357289ec7dc512ae5
Frah01 <vinario.francesco@gmail.com>
2022-11-30 15:25:51 +0100

    Push esercizio dadi iniziato

---
+++ dadi/js/script.js
@@ -0,0 +1,24 @@
+let min = 1
+let max = 6
+let x = Math.floor(Math.random() * (max - min + 1) + min)
+console.log(x)
+let y = Math.floor(Math.random() * (max - min + 1) + min)
+console.log(y)
+
+
+for (let i= 1 ; i<=6; i++){
+    
+    if(x > y){
+        console.log('Il primo giocatore ha vinto')
+        
+    }
+
+    else if (x == y){
+        console.log('Pareggio')
+    }
+    
+    else {
+        console.log ('Il secondo giocatore ha vinto')
+        
+    }
+}
---
+++ dadi/readme.txt
@@ -0,0 +1,6 @@
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+1 - generare un primo numero random da 1 a 6
+2 - generare un secondo numero random da 1 a 6
+    se il primo numero è più grande del secondo, il primo giocatore vincitore
+    se il secondo numero è più grande del primo, il secondo giocatore vince
